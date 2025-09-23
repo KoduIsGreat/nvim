dcb2352802841df389c287c3fa73de4d9820bada
Adam Shelton <adamshelton@rti.org>
2025-09-23 10:24:27 -0400

    supermaven ftw

--- lua/custom/plugins/copilot.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'github/copilot.vim',
-  config = function(self, opts)
-    -- vim.g.copilot_assume_mapped = true
-    -- vim.g.copilot_map_leader = '<leader>'
-    -- vim.g.copilot_map_localleader = '<localleader>'
-  end,
-}
